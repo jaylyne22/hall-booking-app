28d0677b7ebbce5ab39489dda0b5bec073e1a06f
jaylyne22 <jaylyne89@gmail.com>
2026-09-02 16:31:00 +0800

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -3,11 +3,14 @@
 //
 // 这几个值不是密码，可以放在公开的网页里。真正保护资料的是 firestore.rules
 // 里的规则 + 登入帐号，所以规则一定要照 README 部署上去。
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// Import the functions you need from the SDKs you need
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYkEBU0tmbYLRZ1aJmfHsEIAumT6e-dEo",
+  authDomain: "cpt-traning-hall.firebaseapp.com",
+  projectId: "cpt-traning-hall",
+  storageBucket: "cpt-traning-hall.firebasestorage.app",
+  messagingSenderId: "594067332813",
+  appId: "1:594067332813:web:b22f17d3486183970e5384",
+  measurementId: "G-JZEF5YM3TV"
 };
